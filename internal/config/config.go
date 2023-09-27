06eaf69c52ed3f34db16c802d6e3a32182353ed1
Almost <sendominor@gmail.com>
2023-09-27 14:47:45 +0300

    end init, make logger

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,12 @@
 package config
 
-import "time"
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
 
 type Config struct {
 	Env         string `yaml:"env" env:"ENV" env-default:"local" env-required:"true"`
@@ -10,11 +16,27 @@ type Config struct {
 }
 
 type HttpServer struct {
-	Address     string        `yaml:"storage_path" env-default:"localhost:8080"`
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
-	IdleTimeout time.Duration `yaml:"timeout" env-default:"60s"`
+	IdleTimeout time.Duration `yaml:"idle-timeout" env-default:"60s"`
 }
 
-func MustLoad() {
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
 
+	if configPath == "" {
+		log.Fatal("Config path is not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Config file is not found: %s", configPath)
+	}
+
+	var conf Config
+
+	if err := cleanenv.ReadConfig(configPath, &conf); err != nil {
+		log.Fatalf("Cannot read config: %s", err)
+	}
+
+	return &conf
 }
--- main.go
+++ main.go
@@ -1,1 +1,52 @@
-package short_url_api
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"log/slog"
+	"os"
+	"short-url-api/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error in load .env file!")
+	}
+}
+
+func main() {
+	// app conf
+	conf := config.MustLoad()
+
+	log := setupLogger(conf.Env)
+	log.Info("Start url shortener!", slog.String("env", conf.Env))
+	log.Debug("debug msg enabled!")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
